c9f65b4840b5ff6b26f3b2777766fc2daa119387
CNwante <Myphilothea@gmail.com>
2024-01-17 17:28:23 +0100

    File: add monty_stream.c

---
+++ monty_stream.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/*Include betty docs*/
+
+void fopen_failed(char *file_name)
+{
+    fprintf(stderr, "Error: Can't open file%s\n", file_name);
+    /*free_memory();*/
+    exit(EXIT_FAILURE);
+}
+
+/*Include betty docs*/
+void file_stream(char *file_name)
+{
+    int fd;
+
+    fd = open(file_name, O_RDONLY);
+    if (fd == -1)
+    {
+        fopen_failed(file_name);
+    }
+    
+    args->stream = fdopen(fd, "r");
+    if (args->stream == NULL)
+    {
+        close(fd);
+        fopen_failed(file_name);
+    }
+}
