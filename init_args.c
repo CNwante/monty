caf802ec7acf643fac728119949ab462b6e0177b
CNwante <Myphilothea@gmail.com>
2024-01-18 04:19:12 +0100

    Allocate memory and init arguments

--- init_args.c
+++ init_args.c
@@ -8,9 +8,14 @@ void init_args()
 {
     args = malloc(sizeof(argument_t));
     if (args == NULL)
-    {
         malloc_failed();
-    }
+
+    args->instruction = malloc(sizeof(instruction_t));
+    if (args->instruction == NULL)
+        malloc_failed();
+
     args->stream = NULL;
     args->text_line = NULL;
+    args->num_tokens = 0;
+    args->line_number = 0;
 }
