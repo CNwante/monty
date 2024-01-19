7f656a95e6d3399ef5c76e2fc09951375d0235b2
CNwante <myphilothea@gmail.com>
2024-01-18 21:05:57 -0800

    Uncomment get_opcode function

--- instruction.c
+++ instruction.c
@@ -6,28 +6,29 @@ void invalid_opcode(void)
     fprintf(stderr, "L%d: unknown instruction %s\n", args->line_number, args->tokens[0]);
     close_strem();
     free_tokens_mem();
-    /*free_args_mem();*/
+    free_args_mem();
     exit(EXIT_FAILURE);
 }
 
 /*Betty docs goe here*/
 
 void get_opcode(void)
-{/*
+{
     int i = 0;
     instruction_t actions[] = {
         {"push", &push}, {"pop", &pop},
         {"pint", &pint}, {"swap", &swap},
-        {"nop", &nop}, {"add", &add},
-        {"pall", &pall}, {"sub", &sub},
-        {"div", &_div}, {"mul", &mul},
-        {"rotl", &rotl}, {"rotr", &rotr},
-        {"stack", &stack}, {"queue", &queue},
-        {"pstr", &pstr}, {"pchar", &pchar},
-        {"mod", &mod}, {NULL, NULL}
-    }
+ /*       {"nop", &nop}, {"add", &add},*/
+        {"pall", &pall}, /* {"sub", &sub},*/
+     /*   {"div", &_div}, {"mul", &mul},*/
+     /*   {"rotl", &rotl}, {"rotr", &rotr},*/
+    /*    {"stack", &stack}, {"queue", &queue},*/
+   /*     {"pstr", &pstr}, {"pchar", &pchar},*/
+       /* {"mod", &mod},*/
+	       	{NULL, NULL}
+    };
 
-    if args->num_tokens == 0)
+    if (args->num_tokens == 0)
         return;
 
     while (actions[i].opcode != NULL)
@@ -40,5 +41,5 @@ void get_opcode(void)
         }
         i++;
     }
-    invalid_opcode(); */
+    invalid_opcode();
 }
