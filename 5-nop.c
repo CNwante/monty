d149fa3d199f54c65302151d69f98ff9ed09c61f
CNwante <myphilothea@gmail.com>
2024-01-19 17:52:45 -0800

    Task(5): implement the nop opcode
    - The nop opcode does nothing

---
+++ 5-nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - performs no operation
+ * @stack: A pointer to the stack.
+ * @line_number: Line number where the nop function is called.
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
