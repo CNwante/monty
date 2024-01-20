45f3c83b471608222e8780b4bf5f2dd55aeed3ac
CNwante <myphilothea@gmail.com>
2024-01-20 07:36:55 -0800

    Task(15): implement the stack and queue opcodes

---
+++ 15-queue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * queue - Sets the format of the data to a queue (FIFO)
+ * @stack: A pointer to the stack.
+ * @line_number: Line number where the queue function is called.
+ */
+
+void queue(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	args->stack = 0;
+}
---
+++ 15-stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * stack - Sets the format of the data to a stack (LIFO)
+ * @stack: A pointer to the stack.
+ * @line_number: Line number where the stack function is called.
+ */
+
+void stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	args->stack = 1;
+}
