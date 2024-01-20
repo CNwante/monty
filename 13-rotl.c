5142ddaed06320e8fe06835c326e0f22dd8a48b4
CNwante <myphilothea@gmail.com>
2024-01-19 19:50:46 -0800

    Task(13): implement the rotl opcode

---
+++ 13-rotl.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * rotr - Rotates the stack to the bottom
+ * @stack: A pointer to the stack.
+ * @line_number: Line number where the rotl function is called.
+ */
+
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *last;
+
+	(void) stack;
+	(void) line_number;
+	if (args->stack_length < 2)
+		return;
+
+	top = args->head;
+	while (top)
+	{
+		if (top->next == NULL)
+		{
+			last = top;
+			break;
+		}
+		top = top->next;
+	}
+
+	last->prev->next = NULL;
+	last->next = args->head;
+	last->prev = NULL;
+
+	arguments->head = last;
+}
